15a055afafc1a6a9703e2282a418ca793648fbe4
kung-taek <chanpa2167@naver.com>
2025-04-15 05:46:47 +0900

    ㅅㅅㅅ

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -7,7 +7,7 @@ const router = express.Router();
 // 회원가입
 router.post('/signup', (req, res) => {
     console.log('🔥 /signup 요청 도착! 했냐?');
-    res.send('회원가입 성공이라고 해라 제발');
+    res.send('회원가입 성공이라고 해');
 });
 
 // 로그인
