7e706ffb735d688311a22ee2c3ff28f666c862ad
kung-taek <chanpa2167@naver.com>
2025-04-18 17:46:22 +0900

    qwe

--- backend/src/models/database.js
+++ backend/src/models/database.js
@@ -12,7 +12,7 @@ dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 
 // MySQL 연결 설정
 const pool = mysql.createPool({
-    host: process.env.DB_HOST || 'globbalhelperusers.c1sammc8adim.ap-northeast-2.rds.amazonaws.com',
+    host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || '',
     database: process.env.DB_NAME || 'AWS_DB',
