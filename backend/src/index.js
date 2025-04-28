4d201bc50c65447295587828948232630e556d67
kung-taek <chanpa2167@naver.com>
2025-04-28 09:13:59 +0900

    ㄹㄹ

--- backend/src/index.js
+++ backend/src/index.js
@@ -37,7 +37,7 @@ app.use(
     cors({
         origin: function (origin, callback) {
             if (!origin) {
-                // 서버-서버 통신 (postman, curl 등) 허용
+                // 서버-서버 통신 허용
                 return callback(null, true);
             }
             if (allowedOrigins.includes(origin)) {
