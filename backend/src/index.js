587d0fd6eb632dbae1921c0fa2de9cf7a78fe579
kung-taek <chanpa2167@naver.com>
2025-05-02 10:47:51 +0900

    ㄱㄱㄷㄷㅈㅈㅂㅂㅂ

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
 import cors from 'cors';
 import dotenv from 'dotenv';
 import path from 'path';
