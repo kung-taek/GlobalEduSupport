e41566feac4b788fbbe8399748fd835605868f57
kung-taek <chanpa2167@naver.com>
2025-05-12 17:15:33 +0900

    dd

--- backend/src/middleware/authMiddleware.js
+++ backend/src/middleware/authMiddleware.js
@@ -1,16 +1,16 @@
-const jwt = require('jsonwebtoken');
+const jwt = 'jsonwebtoken';
 
 const authenticateToken = (req, res, next) => {
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
 
-    console.log("Received Token:", token);  // ✅ 토큰 출력
+    console.log('Received Token:', token); // ✅ 토큰 출력
 
     if (!token) return res.status(403).json({ error: 'Access Denied' });
 
     jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
         if (err) {
-            console.log("JWT Verification Error:", err);
+            console.log('JWT Verification Error:', err);
             return res.status(403).json({ error: 'Access Denied' });
         }
         req.user = user;
